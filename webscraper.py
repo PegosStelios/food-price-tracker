a54a5d3f4803978ee22b2d0e090606bc9ed66603
PegosStelios <stelios@pegos.net>
2023-04-21 01:20:59 +0300

    Fixed

--- webscraper.py
+++ webscraper.py
@@ -20,28 +20,45 @@ getPage()
 # Find every categories_title in the file using BeautifulSoup.
 doc = open('sklavenitis-katigories.html', encoding="utf-8")
 soup = BeautifulSoup(doc, "html.parser")
-categories = soup.find_all("h3", class_="categories_title")
+categories = soup.find_all("div", class_="categories_item")
 
-# Add all the categories to a list purging the the spaces.
-categories_list = []
-for category in categories:
-    categories_list.append(category.text.strip())
 
-# Make a dictionary with the categories as keys and empty lists as values.
+# Loop through each category and print the category name, image and subcategories.
+# Each category should be its own dictionary with the following keys: name, image, subcategories. (Use the name of the category as the key.)
+# The subcategories should be a list of dictionaries with the following keys: name, link.
+
 categories_dict = {}
-for category in categories_list:
-    categories_dict[category] = []
+categories_list = []
 
-# Find all dropdown menus with class name 'categories_subs'
-dropdown_menus = soup.find_all('div', {'class': 'categories_subs'})
+for category in categories:
+    category_name = category.find("h3", class_="categories_title").text.strip()
+    category_image = category.find("img")["src"].strip().replace("\n", "").replace("\t", "")
+    subcategories = category.find("div", class_="categories_subs")
+    subcategories_list = []
 
-# Loop through each dropdown menu and extract the data
-for dropdown_menu in dropdown_menus:
-    menu_items = dropdown_menu.find_all('a')
-    for item in menu_items:
-        print(item.text.strip())
+    for subcategory in subcategories.find_all("a"):
+        subcategory_name = subcategory.text.strip()
+        subcategory_link = subcategory["href"].strip()
 
+        subcategories_list.append({
+            "name": subcategory_name,
+            "link": subcategory_link
+        })
 
-# The code above scrapes all the categories items from the website.
-# Each time we move to a new category (we can tell we moved since each category has a unique image),
-# save all it's items to a dictionary with the categories_list values as keys.
+    categories_dict[category_name] = {
+        "image": category_image,
+        "subcategories": subcategories_list
+    }
+
+    categories_list.append(categories_dict)
+
+# It should look like this
+# categories_dict = {
+#     category_name = {
+#         "image": image,
+#         "subcategories": subcategories_list
+#     }
+# }
+
+#print(categories_dict)
+print(categories_dict["Είδη Αρτοζαχαροπλαστείου"])
