e3db8a9b6f14fc36207d0c63cc97d06284806eca
PegosStelios <stelios@pegos.net>
2023-04-28 13:10:08 +0300

    fix: proper main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sklavenitis
 
 if __name__ == '__main__':
+    sklavenitis.create_folder()
+    sklavenitis.create_categories()
     sklavenitis.get_cookie_playwright()
     sklavenitis.send_and_save_requests()
 
